9b8ebead402cce72c9654c8b6eeadfd9b6cde1a5
Pranita Gandhi <pranita_gandhi@hotmail.com>
2019-05-17 08:48:32 +0000

    Added from validation - user login and logout routes

--- main.py
+++ main.py
@@ -26,27 +26,57 @@ def index():
     
 @app.route('/register', methods=['GET', 'POST'])
 def register():
- 
- if session.get('logged_in'):
+    if session.get('logged_in'):
         if session['logged_in'] is True:
             return redirect(url_for('index'))
 
     form = RegistrationForm()
-
-
- 
-   
-  
-
-
-
-   
-          
     
+    if form.validate_on_submit():
 
+        user = mongo.db.user
+        dup_user = user.find_one({'name' : request.form['username'].title()})
+        if dup_user is None:
+            user.insert_one({'name' : request.form['username'].title()})
+            session['username'] = request.form['username']
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+
+        flash('Sorry, username already taken. Please try another.')
+        return redirect(url_for('register'))
+
+    return render_template('register.html', form=form, title="Register")
+    
+@app.route('/login', methods=['GET', 'POST'])
+def user_login():
+    if session.get('logged_in'):
+        if session['logged_in'] is True:
+            return redirect(url_for('index'))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = mongo.db.user
+        logged_in_user = user.find_one({'name' : request.form['username'].title()})
+
+        if logged_in_user is None:
+            flash('Incorrect username, please try again')
+            return redirect(url_for('user_login'))
+        session['username'] = request.form['username']
+        session['logged_in'] = True
+        return redirect(url_for('index'))
+
+    return render_template('login.html', form=form, title="Login")
+
+
+@app.route('/logout')
+def logout():
+    """Logs the user out and redirects to home"""
+    session.clear()
+    return redirect(url_for('index'))    
 
  if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-    port=int(os.environ.get("PORT")),
-    debug=True)
+   app.run(host=os.environ.get("IP"),
+   port=int(os.environ.get("PORT")),
+    
    
