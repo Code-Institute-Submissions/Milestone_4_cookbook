c7ff279f660eda9d6e73fea57c9a064e2e86a10e
Pranita Gandhi <pranita_gandhi@hotmail.com>
2019-05-17 07:16:19 +0000

    Added secret key

--- env.py
+++ env.py
@@ -3,3 +3,5 @@ import os
 # Store MONGO_URI as an environmental variable
 
 os.environ["MONGO_URI"]= 'mongodb+srv://root:RootUser@myfirstdatabase-klrg6.mongodb.net/myCookBook?retryWrites=true'
+
+os.environ["SECRET_KEY"] = 'it-is-a-secret'
--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+# Importing Flask,PyMongo
+
 import os,env
 from flask import Flask,render_template,redirect,request,url_for
 from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] ="myCookBook"
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI', 'monogodb://localhost')
+
+
+
+mongo = PyMongo(app)
+
+# Routing
+
+@app.route('/')
+@app.route('/get_recipes')
+def home():
+          
+    
+
+
+ if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+    port=int(os.environ.get("PORT")),
+    debug=True)
+   
