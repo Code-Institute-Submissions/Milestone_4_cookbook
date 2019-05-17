9cff8b3baa779646fccade242ae1d732c67b12e5
Pranita Gandhi <pranita_gandhi@hotmail.com>
2019-05-17 07:36:43 +0000

    Added routing- GET and POST  for form registration

--- form.py
+++ form.py
@@ -1,1 +1,13 @@
 from flask_wtf import FlaskForm
+from wtfroms import StringField, BooleanField, SubmitField
+from wtforms.validators import DateRequired, ValidationError
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+    
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    submit = SubmitField('Register')    
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # Importing Flask,PyMongo
 
 import os,env
-from flask import Flask,render_template,redirect,request,url_for
+from flask import Flask, render_template, redirect, request, url_for, flash, session
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] ="myCookBook"
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI', 'monogodb://localhost')
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 
 
 
@@ -17,7 +18,29 @@ mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/get_recipes')
-def home():
+def index():
+    recipe = mongo.db.recipe.find()
+    return render_template('index.html', recipe=recipe, title="Home"
+    
+# Routing - Registration of login    
+    
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+ 
+ if session.get('logged_in'):
+        if session['logged_in'] is True:
+            return redirect(url_for('index'))
+
+    form = RegistrationForm()
+
+
+ 
+   
+  
+
+
+
+   
           
     
 
