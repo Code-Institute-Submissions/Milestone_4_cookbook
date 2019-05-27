ecf1219f1b2d4c4794295a6811f3e6508803f715
Pranita Gandhi <pranita_gandhi@hotmail.com>
2019-05-27 16:54:59 +0000

    CSS added for containers - html classes to be added

---
+++ static/js/main.js
@@ -0,0 +1,5 @@
+// Materialize Sidenav
+document.addEventListener('DOMContentLoaded', function() {
+    const elems = document.querySelectorAll('.sidenav');
+    const instances = M.Sidenav.init(elems);
+  });
