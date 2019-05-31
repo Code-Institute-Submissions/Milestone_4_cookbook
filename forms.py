c564115409c6747b6fd5c7819daba63b7bad72bb
Pranita Gandhi <pranita_gandhi@hotmail.com>
2019-05-31 11:35:46 +0000

    Fixed sticky footer and added icon for toggle button to work

--- forms.py
+++ forms.py
@@ -3,7 +3,8 @@ from wtforms import StringField, TextAreaField, IntegerField, PasswordField
 from wtforms.validators import DataRequired, ValidationError, Optional
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username',validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
   
 
 class RegistrationForm(FlaskForm):
